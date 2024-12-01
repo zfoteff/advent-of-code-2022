3d33bdb5313088d5c009a6108a5bdc1c596e8f46
zfoteff <zfoteff@protonmail.com>
2024-11-30 20:20:54 -0800

    update 2024 to main

--- 2024/main.py
+++ 2024/main.py
@@ -1,13 +1,18 @@
+#!/usr/bin/python3
+
 from utils.timed_runner import timed_advent_of_code_runner
 
 from pkg.day1 import part_one, part_two, import_puzzle_data
 
 
 def main():
+    """Runner file for 2024 advent of code submissions. Returns all puzzles with 
+    """
+
     # Day 1
     timed_advent_of_code_runner(
         "Day 1", 
-        "data/day-1-puzzle-data.txt", 
+        "./data/day-1-puzzle-data.txt", 
         part_one, 
         part_two, 
         import_puzzle_data
